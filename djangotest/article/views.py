19faeeb125d1ed907fb8ac938e20df752f20aa5f
thangabalu <thangabalu@gmail.com>
2014-06-27 01:47:17 +0200

    adding table in show recipe

--- djangotest/article/views.py
+++ djangotest/article/views.py
@@ -4,6 +4,8 @@ from django.template import Context
 from django.shortcuts import render_to_response
 from article.models import Article
 from django.http import HttpResponseRedirect
+from django.utils.datastructures import SortedDict
+import re
 
 # Create your views here.
 #Order.objects.order_by('-date')[0]
@@ -18,10 +20,26 @@ def recipetype (request, recipetype):
 				 'recipes': Article.objects.all()})
 
 def showrecipe (request, recipetitle=""):
+    recipe = Article.objects.get(title=recipetitle.replace("-"," "))
+    ingredients = recipe.ingredients
+    ingredients_split = ingredients.split('\n')    
+    ingredient_dictionary= SortedDict()
+    for ingredient in ingredients_split:
+        if '<h>' in ingredient:
+	    match = re.search('<h>(.*)',ingredient)
+   	    if match:
+	        ingredient_dictionary[match.group(1)] =[]
+		key=match.group(1)
+	elif ';' in ingredient:
+	    match = re.search('(.*);(.*)',ingredient)
+	    if match:
+	        ingredient_dictionary[key].append([match.group(1),match.group(2)])
+	    
     return render_to_response('show_recipe.html',
 		{'article': Article.objects.get(title=recipetitle.replace("-"," ")),
 		 'latest_recipes_ten' : Article.objects.all().order_by('-pub_date')[:10],
 		 'popular_recipes_ten' : Article.objects.all().order_by('-likes')[:10],
+		 'ingredient'	     :ingredient_dictionary
 		})
 
 def recipes_all(request):
