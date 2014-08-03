a93472648eff4b0825d150a51f54a69d0464bf9c
thangabalu <thangabalu@gmail.com>
2014-08-03 18:34:48 +0200

    mouse leave event for editable div placeholder

--- djangotest/static/js/first.js
+++ djangotest/static/js/first.js
@@ -1,9 +1,14 @@
 $(document).ready(function(){
-  
+   //When mouse key is down on the editable name div, it empties the content
    $(".content_block_show_recipe").on("mousedown", ".editable_div", function(){
       $(this).empty();
    });  
 
+   //When mouse key leaves the editable name div, it fills the content back
+   $(".content_block_show_recipe").on("mouseleave", ".editable_div", function(){
+      $(this).text('Enter your name')
+   });     
+   
 });
 
 
@@ -18,12 +23,14 @@ function you_hovered(name,comment_id,recipe_type,recipe_title_url_format,comment
         $("<form/>",{action:'/recipes/comments/reply/', method:'POST',name: 'form'}).append(
         //Replacing text area with editable div. Since autoexpanding is tough in that.
         $("<div/>",{class:"textbox_button_div"}).append(
+        $("<div />").css("margin-bottom","10px"), //br tag is not working. Using div tag for line space
+        $("<span/>").text("You are replying to the above comment"),
         $("<div/>", {class:"editable_div", id:"editable_div_name_"+name, text:"Enter your name",contenteditable: "true", width:"325px",onkeypress: "return (this.innerText.length <= 50)" }).css("border","thin solid black"),
         $("<div />").css("margin-bottom","5px"), //br tag is not working. Using div tag for line space
         $("<div/>", {id:"editable_div_comment_"+name, text:"@"+commenter_name+":",contenteditable: "true", width:"325px",onkeypress: "return (this.innerText.length <= 256)" }).css("border","thin solid black"),        
-        $("<div />").css("margin-bottom","5px"), //br tag is not working. Using div tag for line space
+        //$("<div />").css("margin-bottom","5px"), //br tag is not working. Using div tag for line space
         $("<input/>", {type:'button', id:"mybutton_"+name, value:'Submit'}),
-        $("<div />").css("margin-bottom","5px"), //br tag is not working. Using div tag for line space
+        //$("<div />").css("margin-bottom","5px"), //br tag is not working. Using div tag for line space
         $("<input/>", {type:'hidden', name:'recipe_type', value:recipe_type}),
         $("<input/>", {type:'hidden', name:'comment_id', value:comment_id}),
         $("<input/>", {type:'hidden', name:'recipe_title_url_format', value:recipe_title_url_format})
